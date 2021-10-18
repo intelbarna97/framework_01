a4cff7c3bf406ea07867a97233d819efc6c6d90f
Vereb Barna <intelbarna97@gmail.com>
2021-10-18 10:04:37 +0200

    framework project

---
+++ test2/src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.example.dao.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App implements CommandLineRunner
+{
+    @Autowired
+    private ApplicationContext context;
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Hello World");
+        context.getBean(PlayerRepository.class).findAll().stream().limit(10).forEach(System.out::println);
+    }
+}
